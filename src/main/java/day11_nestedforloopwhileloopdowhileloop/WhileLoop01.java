36a6c79574fe99424b63f126793c5754bfe5d673
fuatyagmur <fuatyagmurr@gmail.com>
2023-01-27 02:32:22 +0100

    27

--- src/main/java/day11_nestedforloopwhileloopdowhileloop/WhileLoop01.java
+++ src/main/java/day11_nestedforloopwhileloopdowhileloop/WhileLoop01.java
@@ -79,6 +79,28 @@ public class WhileLoop01 {
             r++;
         }
 
+         /*
+             Example 5: Type java code by using while loop,
+             Write a program that prompts the user to input a number.
+             It should then print the multiplication table of that number.
+             3x1=3  3x2=6  3x3=9  3x4=12 3x5=15  3x6=18  3x7=21  3x8=24  3x9=27  3x10=30
+        */
+
+        System.out.println("Please enter an integer to create multiplication table...");
+        int f= input.nextByte();
+
+        int y=1;
+
+        while (y<11){
+
+            System.out.println(f + "x" + y + "=" + (f*y) );
+
+            y++;
+        }
+
+
+
+
 
     }
 

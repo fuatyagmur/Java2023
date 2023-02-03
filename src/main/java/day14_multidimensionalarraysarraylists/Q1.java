79846ae9a0d9de30fc682b9d192d5e4534326c7c
fuatyagmur <fuatyagmurr@gmail.com>
2023-02-03 15:32:03 +0100

    3

--- src/main/java/day14_multidimensionalarraysarraylists/Q1.java
+++ src/main/java/day14_multidimensionalarraysarraylists/Q1.java
@@ -93,6 +93,51 @@ public class Q1 {
 
 
 
+        //Example 1: Convert 2 dimensional array to 1 dimensional array
+        //           [ [5, 3], [9, 8, 7] ] ==> [5, 3, 9, 8, 7]
+        //1. Step: Find the number of elements in the multidimensional array
+        //2. Step: Create new 1 dimensional array whose length is the same
+        //         with number of elements in the multidimensional array
+        //3. Step: Transfer the elements from the multidimensional array to the 1 dimensional array
+
+        //1. Step: Find the number of elements in the multidimensional array
+
+        int g[][]={{0,1,2,3},{4,5},{6,7,8,9}};
+
+        int sayac=0;
+
+        for (int []w:g) {
+
+            sayac = sayac + w.length;
+
+        }
+
+        System.out.println("sayac = " + sayac);
+
+        //2. Step: Create new 1 dimensional array whose length is the same
+        //         with number of elements in the multidimensional array
+
+        int h[]=new int[sayac];
+        System.out.println(Arrays.toString(h));  //[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        System.out.println(h.length);  //10
+
+        //3. Step: Transfer the elements from the multidimensional array to the 1 dimensional array
+
+        int index2=0;
+
+        for (int []w:g) {
+            for (int j:w) {
+
+                h[index2]=j;
+
+                index2++;
+
+            }
+
+        }
+
+        System.out.println(Arrays.toString(h));
+
 
 
 

0aa5c59c58709cb821cd7d3302582c639b66eca6
fuatyagmur <fuatyagmurr@gmail.com>
2023-02-07 12:14:31 +0100

    7

--- src/main/java/practices/array/array03.java
+++ src/main/java/practices/array/array03.java
@@ -59,6 +59,50 @@ public class array03 {
 
 
 
+        //Find the smallest positive element and greatest negative element in an integer array
+        //  Example:(-12, 18, -5, 23, -2) ==> Smallest positive is 18, the greatest negative is -2
+
+        int [] c=new int[]{3,6,-2,-6,17,9};
+        Arrays.sort(c);
+        System.out.println(Arrays.toString(c));  //[-6, -2, 3, 6, 9, 17]
+
+        //1.way: for each loop
+        minPositive=c[c.length-1];
+        maxNegative=c[0];
+
+        for (int w:c) {
+
+            if (w<0){
+              maxNegative= Math.max(maxNegative,w);
+            }
+
+            if (w>0){
+                minPositive=Math.min(minPositive,w);
+            }
+
+        }
+
+        System.out.println("maxNegative = " + maxNegative);  //maxNegative = -2
+        System.out.println("minPositive = " + minPositive);  //minPositive = 3
+
+
+        //2.way: for loop
+
+        int[] d=new int[]{-6, -2, 3, 6, 9, 17};
+        Arrays.sort(d);
+        System.out.println(Arrays.toString(d));
+
+        for (int i = 0; i <d.length ; i++) {
+
+            if (d[i]<0 && d[i+1]>0){
+                System.out.println("MaxNegativeD: "+ d[i]);  //MaxNegativeD: -2
+                System.out.println("MinPositiveD: "+d[i+1]);  //MinPositiveD: 3
+
+            }
+
+        }
+
+
 
 
 

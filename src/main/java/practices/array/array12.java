b17c8bc73c9436161b73e1f42fa822486c7d9229
fuatyagmur <fuatyagmurr@gmail.com>
2023-02-10 02:22:55 +0100

    10

--- src/main/java/practices/array/array12.java
+++ src/main/java/practices/array/array12.java
@@ -1,5 +1,7 @@
 package practices.array;
 
+import java.util.Arrays;
+
 public class array12 {
 
     public static void main(String[] args) {
@@ -13,6 +15,20 @@ public class array12 {
             sum = sum + w.length(); }
         System.out.println(sum);
 
+
+        //Type code to find sum of the number of characters of array elements
+
+        String[] fruits = new String[]{"apple", "banana", "cherry", "date", "elderberry"};
+
+       int total=0;
+
+        for (String j:fruits) {
+
+            total=total + j.length();
+
+        }
+
+        System.out.println("total = " + total);
 
 
 

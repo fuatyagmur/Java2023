5e643cc38a8115b8a55bb6f08f1c7d1810a809d4
fuatyagmur <fuatyagmurr@gmail.com>
2023-02-10 02:22:44 +0100

    10

--- src/main/java/practices/array/array11.java
+++ src/main/java/practices/array/array11.java
@@ -6,7 +6,11 @@ public class array11 {
 
         //Type code to find if a given element exists in a given array or not
 
-        String[] arr = new String[]{"Apex", "is", "an", "object", "oriented", "programming", "language"}; String s = "object";
+        String[] arr = new String[]{"Apex", "is", "an", "object", "oriented", "programming", "language"};
+        String s = "object";
+
+        //1.way
+
         int counter = 0;
         for(String w : arr){
             if(w.equalsIgnoreCase(s)){ counter++;
@@ -14,7 +18,60 @@ public class array11 {
         if(counter>0){ System.out.println(s + " exists");
         }else{
             System.out.println(s + " does not exist");
+
         }
+
+        //2.way
+
+        //Type code to find if a given element exists in a given array or not
+
+        String[] arr2 = new String[]{"Apex", "is", "an", "object", "oriented", "programming", "language"};
+        String s2 = "object";
+
+        boolean elementExists=false;
+
+        for (String w:arr2) {
+
+            if (w.equals(s2)) {
+                elementExists = true;
+                break;
+            }
+        }
+
+            if (elementExists){
+                System.out.println("exist");
+            }else {
+                System.out.println("doesnt exist");
+            }
+
+
+
+
+
+
+        //3.way
+        String[] arr3 = new String[]{"Apex", "is", "an", "object", "oriented", "programming", "language"};
+        String s3 = "object";
+
+
+        int counter3=0;
+
+        for (String w:arr3) {
+
+            if (w.equalsIgnoreCase(s3)){
+                counter3++;
+                //System.out.println("Exist3");  --> Exist3
+            }
+
+        }
+
+        if (counter3>0){
+            System.out.println("Exists3");
+        }else {
+            System.out.println("Doesn't exists3");
+        }
+
+
 
 
 

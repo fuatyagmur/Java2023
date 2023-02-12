ef65629ec365e540cb70f079a9de439bc108f5a0
fuatyagmur <fuatyagmurr@gmail.com>
2023-02-12 19:20:04 +0100

    12

---
+++ src/main/java/practices/lists/lists04.java
@@ -0,0 +1,52 @@
+package practices.lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class lists04 {
+
+    public static void main(String[] args) {
+
+//        Check if elements are in descending order in a list
+//        Example: (Yellow, Blue, Red, Green) ==> Output: It is not in descending order
+//        (Yellow, Red, Green, Blue) ==> Output: It is in descending order
+
+        List<String > e=new ArrayList<>();
+        e.add("Yellow");
+        e.add("Blue");
+        e.add("Red");
+        e.add("Green");
+
+        System.out.println("e = " + e);  //e = [Yellow, Blue, Red, Green]
+
+        List<String> f=new ArrayList<>();
+        f.addAll(e);
+
+        Collections.sort(f);
+
+        System.out.println("e = " + e);  //e = [Yellow, Blue, Red, Green]
+        System.out.println("f = " + f);  //f = [Blue, Green, Red, Yellow]
+
+        int size=e.size();
+        int flag=0;
+
+        for (int i = 0; i <size ; i++) {
+            if (f.get(i).equals(e.get(size-1-i))){
+                flag++;
+            }
+
+        }
+
+        if (flag==size){
+            System.out.println("It is in descending order");
+        }else {
+            System.out.println("It is not in descending order");
+        }
+
+
+
+
+
+    }
+}

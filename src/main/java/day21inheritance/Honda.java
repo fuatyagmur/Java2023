a66ab5c33bbcb292f4b60f27ac1074d2654f9df3
fuatyagmur <fuatyagmurr@gmail.com>
2023-02-24 22:57:17 +0100

    24

---
+++ src/main/java/day21inheritance/Honda.java
@@ -0,0 +1,13 @@
+package day21inheritance;
+
+public class Honda extends Vehicle{
+
+    public void engine(){
+        System.out.println("Honda uses perfect engine...");
+    }
+
+    public void brake(){
+        System.out.println("Honda stops securely...");
+    }
+
+}

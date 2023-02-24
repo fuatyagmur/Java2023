b11cb035e2a6d50a97feed6379dc80bcea973c1c
fuatyagmur <fuatyagmurr@gmail.com>
2023-02-24 22:57:08 +0100

    24

---
+++ src/main/java/day21inheritance/Civic.java
@@ -0,0 +1,12 @@
+package day21inheritance;
+
+public class Civic extends Honda {
+
+    public void ecoSystem(){
+        System.out.println("Using Civic is not expensive...");
+    }
+
+    public Civic() {
+        System.out.println("Just Civic");
+    }
+}

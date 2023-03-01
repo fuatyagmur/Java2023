b47afb95c272cbdc3e4b67f83b9efea7f95acd9b
fuatyagmur <fuatyagmurr@gmail.com>
2023-03-01 23:39:15 +0100

    01

---
+++ src/main/java/day22overridingencapsulation/AnimalRunner.java
@@ -0,0 +1,14 @@
+package day22overridingencapsulation;
+
+public class AnimalRunner {
+
+    public static void main(String[] args) {
+
+        Cat myCat=new Cat();
+
+        myCat.eat();   //Animals eat...
+        myCat.feed();  //Mammals feed their babies with milk...
+
+    }
+
+}

102a4511df2538213728dfe492e5e91347f04545
fuatyagmur <fuatyagmurr@gmail.com>
2023-03-04 23:45:53 +0100

    04

---
+++ src/main/java/day23encapsulationabstraction/StudentRunner.java
@@ -0,0 +1,18 @@
+package day23encapsulationabstraction;
+
+public class StudentRunner {
+
+    public static void main(String[] args) {
+
+        Student myStd = new Student();
+
+        String psycho1 = myStd.getPsychologicalIssues();
+        System.out.println(psycho1);// Depression
+
+
+
+
+
+
+    }
+}

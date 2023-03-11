7f646136b890cd292db26d656b8014f968a34724
fuatyagmur <fuatyagmurr@gmail.com>
2023-03-11 17:03:38 +0100

    11

---
+++ src/main/java/day25collections/Sets01.java
@@ -0,0 +1,29 @@
+package day25collections;
+
+public class Sets01 {
+
+     /*
+        Sets are for storing unique data.
+        There are 3 sets i)HashSet: Uses hashing technique.
+                                    Hashing technique is a technique to create unique data(code)
+                                    HashSet does not put the elements in any order, so HashSet is so fast
+                                    HashSet allows you to store just one "null" as value
+                         ii)LinkedHashSet
+                         iii)TreeSet
+     */
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}

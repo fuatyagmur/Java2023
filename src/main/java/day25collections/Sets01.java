aa8c428a7dfe03f2b53a4e40ca310210b2cea419
fuatyagmur <fuatyagmurr@gmail.com>
2023-03-14 19:05:12 +0100

    14

--- src/main/java/day25collections/Sets01.java
+++ src/main/java/day25collections/Sets01.java
@@ -1,5 +1,7 @@
 package day25collections;
 
+import java.util.HashSet;
+
 public class Sets01 {
 
      /*
@@ -14,7 +16,19 @@ public class Sets01 {
 
     public static void main(String[] args) {
 
+        HashSet<Integer> hs=new HashSet<>();
 
+        hs.add(12);
+        hs.add(3);
+        hs.add(14);
+        hs.add(5);
+        hs.add(34);
+        hs.add(63);
+        hs.add(6);
+        hs.add(23);
+        hs.add(14);
+
+        System.out.println("hs = " + hs);  //hs = [34, 3, 5, 6, 23, 12, 14, 63]
 
 
     }

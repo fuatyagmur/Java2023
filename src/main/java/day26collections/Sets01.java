a8b2aba864e06082c325434caa7928ec67368af4
fuatyagmur <fuatyagmurr@gmail.com>
2023-03-16 23:52:49 +0100

    16

--- src/main/java/day26collections/Sets01.java
+++ src/main/java/day26collections/Sets01.java
@@ -2,6 +2,7 @@ package day26collections;
 
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class Sets01 {
 
@@ -89,6 +90,64 @@ public class Sets01 {
 
             //Note: retainAll() can be used with HashSet, LinkedHashSet, and TreeSet as well
          */
+
+
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.add(13);
+        ts.add(2);
+        ts.add(24);
+        ts.add(19);
+        ts.add(5);
+        ts.add(-3);
+        System.out.println(ts);
+
+        /*
+            Interview Question: What do you use to store unique elements in natural order?
+                                Answer: TreeSet
+
+                                But as you know TreeSets are so slow, how can you prevent your code works slowly?
+                                Answer: Create HashSet, add elements into the HashSet, then convert the HashSet to TreeSet
+         */
+
+        //Example: Store the following email addresses in alphabetical order
+        //         abc@gmail.com, caf@gmail.com, acd@gmail.com, bcm@gmail.com, chi@gmail.com
+
+        //1.Way
+        long t1 = System.nanoTime();
+
+        TreeSet<String> ts1 = new TreeSet<>();
+        ts1.add("abc@gmail.com");
+        ts1.add("caf@gmail.com");
+        ts1.add("acd@gmail.com");
+        ts1.add("bcm@gmail.com");
+        ts1.add("chi@gmail.com");
+        ts1.add("adc@gmail.com");
+        ts1.add("cfs@gmail.com");
+        ts1.add("cds@gmail.com");
+        ts1.add("asc@gmail.com");
+        ts1.add("gnh@gmail.com");
+        System.out.println(ts1);
+
+        long t2 = System.nanoTime();
+        //2.Way
+        HashSet<String> yourHs = new HashSet<>();
+        yourHs.add("abc@gmail.com");
+        yourHs.add("caf@gmail.com");
+        yourHs.add("acd@gmail.com");
+        yourHs.add("bcm@gmail.com");
+        yourHs.add("chi@gmail.com");
+        yourHs.add("adc@gmail.com");
+        yourHs.add("cfs@gmail.com");
+        yourHs.add("cds@gmail.com");
+        yourHs.add("asc@gmail.com");
+        yourHs.add("gnh@gmail.com");
+        TreeSet<String> ts2 = new TreeSet<>(yourHs);
+        System.out.println(ts2);
+
+        long t3 = System.nanoTime();
+
+        System.out.println("Just TreeSet: " + (t2 - t1));
+        System.out.println("HashSet with TreeSet: " + (t3 - t2));
 
 
     }

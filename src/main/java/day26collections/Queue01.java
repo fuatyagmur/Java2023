01e264d95998307f42268cca4b8d6eda5dcddeb3
fuatyagmur <fuatyagmurr@gmail.com>
2023-03-17 23:36:51 +0100

    17

---
+++ src/main/java/day26collections/Queue01.java
@@ -0,0 +1,15 @@
+package day26collections;
+
+public class Queue01 {
+
+    /*
+        1)Queues use First In First Out(FIFO) rule
+        2)Deque stands for Double Ended Queue it uses FIFO and LIFO(Last In First Out)
+     */
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
